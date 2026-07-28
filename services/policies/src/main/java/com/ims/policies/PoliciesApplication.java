06784e75ebca7a5ee35bf3119d954937b2b62264
Venkata Kiran Jakkapu <venkatakiran.jakkapu@gmail.com>
2026-07-28 19:09:52 +0530

    feat(policies): new policies service added to the application architecture

---
+++ services/policies/src/main/java/com/ims/policies/PoliciesApplication.java
@@ -0,0 +1,15 @@
+package com.ims.policies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+
+@SpringBootApplication
+@RefreshScope
+public class PoliciesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoliciesApplication.class, args);
+	}
+
+}
